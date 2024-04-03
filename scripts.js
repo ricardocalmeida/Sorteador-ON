8c665dfd1668d4bb17998e1712634d8dfaebfc06
Ricardo Almeida <ricardocalmeida28@gmail.com>
2024-04-03 03:58:07 -0300

    Ajuste no max e min

--- scripts.js
+++ scripts.js
@@ -3,7 +3,12 @@ function generateNumber(){
     const min = Math.ceil(document.querySelector(".input-min").value)
     const max = Math.floor(document.querySelector(".input-max").value)
 
+    if (min >= max) {
+        alert("O valor mínimo tem que ser MENOR que o valor máximo")
+    }
+    else {
     const result = Math.floor(Math.random() *  max - min +1 ) + min;
     
     alert(result)
+}
 }
